85f87ea2e6d9bece6578e3540eb2f06d2ab2def4
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-15 19:11:18 +0100

    update

--- Pari_e_dispari/script.js
+++ Pari_e_dispari/script.js
@@ -28,15 +28,27 @@ function randomNumber(min,max){
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+// FUNZIONE CHECK PARI O DISPARI
+function podCheck(number){
+    let result = '';
+    if(number % 2 == 0) {
+        return result;
+    }else {
+        return result;
+    }
+}
+
 
 // ADD EVENT LISTENER SUL BOTTONE
 button.addEventListener("click", function(){
-    let userNumber = number.value;
+    let userNumber = parseInt(number.value);
     console.log(userNumber + ": userNumer");
     const podUser = pod.value;
     console.log(podUser);
 
-    const randomNumberCpu = randomNumber(1,5);
+    const randomNumberCpu = parseInt(randomNumber(1,5)) ;
     console.log (randomNumberCpu);
-
+    
+    const sum = randomNumberCpu + userNumber;
+    console.log(sum);
 });
