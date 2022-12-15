e8b2fa8fb96974eb2b912f13d77b386c6211a6dd
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-15 16:17:14 +0100

    apha palindrome

--- palindromo/script.js
+++ palindromo/script.js
@@ -5,3 +5,36 @@ console.log("JS OK");
 // Chiedere all’utente di inserire una parola tramiite un form
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// PRENDO GLI ELEMENTI DAL DOM
+const name = document.getElementById("text");
+console.log(name);
+const button = document.getElementById("button-check")
+console.log(button);
+
+
+
+
+
+// FUNZIONE REVERS A WORD
+function reverseWord (word){
+    let result = " ";
+    for ( let i = word.length - 1; i >= 0  ; i--)
+    result += word[i];
+    return result;
+}
+
+button. addEventListener("click", function(){
+    let userWord = name.value;
+    console.log(userWord + "= userWord");
+
+    const userWordreverse = reverseWord(userWord);
+    console.log(userWordreverse + "= userwordreverse");
+
+    
+    if(userWord = userWordreverse){
+        alert("La tua parola è un Palindromo!");
+    }else{
+        alert("La tua parola NON è un Palindromo!")
+    }
+
+});
